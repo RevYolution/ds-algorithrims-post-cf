c9522675d5e3fe62ce23b85bb64277d2460fe9a7
Jon Rice <revyolution1120@gmail.com>
2019-11-15 21:04:40 -0800

    Updating repo

---
+++ ds-algorithims-post-cf/challenges/ArrayReverse/ArrayReverse/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ArrayReverse
+{
+    class Program
+    {
+        static void MainArrayReverse(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void ArrayReverseWhileLoop()
+        {
+            int[] startArray = { 4, 8, 2, 3, 9 };
+
+            int start = 0;
+            int end = startArray.Length - 1;
+
+            while (start != end)
+            {
+                int hold = startArray[start];
+                startArray[start] = startArray[end];
+                startArray[end] = hold;
+                start++;
+                end--;
+            }
+
+            Console.WriteLine($"The reversed array from a while loop is [{startArray[0]}, {startArray[1]}, {startArray[2]}, {startArray[3]}, {startArray[4]}]");
+
+        }
+
+        public static void ArrayReverseForLoop()
+        {
+            int[] startArray = { 4, 8, 2, 3, 9 };
+            int[] reversedArray = new int[startArray.Length];
+            int end = startArray.Length - 1;
+
+            for (int i = 0; i < startArray.Length; i++)
+            {
+                reversedArray[end] = startArray[i];
+                end--;
+            }
+
+            Console.WriteLine($"The reversed array from for loop is [{reversedArray[0]}, {reversedArray[1]}, {reversedArray[2]}, {reversedArray[3]}, {reversedArray[4]}]");
+
+        }
+    }
+}
---
+++ ds-algorithims-post-cf/challenges/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void MainBinarySearch(string[] args)
+        {
+        }
+        //Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key.
+        //Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key,
+        //or -1 if the element does not exist.
+
+        public static void BinarySearchArray()
+        {
+            int[] sortedArray = { 1, 2, 3, 4, 5, 6 };
+            int searchKey = 2;
+            int left = 0;
+            int right = sortedArray.Length - 1;
+            int pointer = (left+right)/2;
+
+            while (sortedArray[pointer] != searchKey)
+            {
+                if (sortedArray[pointer]>searchKey)
+                {
+                    right = pointer;
+                    pointer = (right + left) / 2;
+                }
+                if (sortedArray[pointer]<searchKey)
+                {
+                    left = pointer;
+                    pointer = (right + left) / 2;
+                }
+            }
+
+            Console.WriteLine($"The number being searched is at index {pointer}");
+        }
+    }
+}
